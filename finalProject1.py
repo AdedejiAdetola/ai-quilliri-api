c1649995658bdfa12d32728b743a782849940fb7
Adetola Adedeji <60980429+AdedejiAdetola@users.noreply.github.com>
2025-03-26 16:03:37 +0100

    Update finalProject1.py

--- finalProject1.py
+++ finalProject1.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[4]:
-
 import os
 
 
@@ -15,14 +13,6 @@ os.system('pip install chromadb==0.3.26')
 # install tiktoken
 os.system('pip install tiktoken==0.4.0')
 
-
-# In[2]:
-
-
-# os.system('wget -r -A.html -P meddocs https://www.mayoclinic.org/')
-
-
-# In[ ]:
 
 
 # Path to the text file containing links
@@ -44,39 +34,6 @@ links_list = ['https://www.mayoclinic.org/', 'https://www.healthline.com/', 'htt
     # os.system('wget -r -P {output_directory} {link}')
 
 
-# In[11]:
-
-
-# pip install --upgrade langchain
-
-
-# In[18]:
-
-
-# pip install -U langchain-community
-
-
-# In[6]:
-
-
-# pip install unstructured
-
-
-# In[1]:
-
-
-# pip install python-magic
-
-
-# In[2]:
-
-
-# pip install python-magic-bin
-
-
-# In[3]:
-
-
 import magic
 
 def get_file_type(file_path):
@@ -87,14 +44,6 @@ def get_file_type(file_path):
     file_type = detector.from_file(file_path)
 
     return file_type
-
-# Example usage:
-# file_path = r'C:\Users\USER\OneDrive\Documents\ML\final_year\meddocs\www.mayoclinic.org\apps.1'
-# file_type = get_file_type(file_path)
-# print(f"The file type of '{file_path}' is: {file_type}")
-
-
-# In[4]:
 
 
 import magic
@@ -149,27 +98,6 @@ import os
 #                 os.rename(file_path, new_file_path)
 #                 print(f"Renamed '{file_path}' to '{new_file_path}'")
 
-# # Example usage:
-# directory = r'C:\Users\USER\OneDrive\Documents\ML\final_year\meddocs\www.mayoclinic.org'
-# rename_html_files_in_directory(directory)
-
-
-# In[ ]:
-
-
-# from langchain_community.document_loaders import DirectoryLoader
-# from langchain_community.document_loaders import BSHTMLLoader
-
-# path = r'C:\Users\USER\OneDrive\Documents\ML\final_year\meddocs\www.mayoclinic.org'
-# loader = DirectoryLoader(path, glob="**/*.html", loader_cls=BSHTMLLoader, silent_errors=True, show_progress=True)
-# docs = loader.load()
-
-# from langchain_community.document_loaders import DirectoryLoader
-
-# path = r'C:\Users\USER\OneDrive\Documents\ML\final_year\meddocs\www.mayoclinic.org'
-
-# loader = DirectoryLoader(path, silent_errors=True, show_progress=True)
-# docs = loader.load()
 
 import os
 from langchain.document_loaders import DirectoryLoader
@@ -191,11 +119,6 @@ for subdir in subdirs:
     all_docs.extend(docs)  # Append loaded documents to the list
 
 # Now `all_docs` contains documents from all subdirectories
-
-
-
-
-# In[15]:
 
 
 len(all_docs)
@@ -259,68 +182,6 @@ openai_api_key = os.getenv('OPENAI_API_KEY')
 # Optionally, set it in the environment if needed
 os.environ['OPENAI_API_KEY'] = openai_api_key
 
-
-
-
-
-# In[8]:
-
-
-# # Import chroma
-# from langchain.vectorstores import Chroma
-
-# # Import OpenAIEmbeddings
-# from langchain.embeddings.openai import OpenAIEmbeddings
-
-# # Create the embedding function
-# embedding = OpenAIEmbeddings()
-
-# # Load the database from existing embeddings
-# db = Chroma(persist_directory="chroma-data-langchain-docs", embedding_function=embedding)
-
-
-# In[ ]:
-
-
-
-
-
-# In[35]:
-
-
-# # import
-# from langchain_chroma import Chroma
-# from langchain_community.document_loaders import TextLoader
-# from langchain_community.embeddings.sentence_transformer import (
-#     SentenceTransformerEmbeddings,
-# )
-# # from langchain_text_splitters import CharacterTextSplitter
-
-# # # load the document and split it into chunks
-# # loader = TextLoader("../../modules/state_of_the_union.txt")
-# # documents = loader.load()
-
-# # # split it into chunks
-# # text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-# # docs = text_splitter.split_documents(documents)
-
-# # create the open-source embedding function
-# embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
-
-# # load it into Chroma
-# db = Chroma.from_documents(documents, embedding_function)
-
-# # query it
-# query = "What did the president say about Ketanji Brown Jackson"
-# docs = db.similarity_search(query)
-
-# # print results
-# print(docs[0].page_content)
-
-
-# In[22]:
-
-
 from langchain_chroma import Chroma
 from langchain_community.document_loaders import TextLoader
 from langchain_community.embeddings.sentence_transformer import (
@@ -330,16 +191,6 @@ from langchain_community.embeddings.sentence_transformer import (
 from tqdm import tqdm
 
 print("Imports completed successfully.")
-
-# load the document
-# loader = TextLoader("../../modules/state_of_the_union.txt")
-# documents = loader.load()
-print("Document loaded successfully.")
-
-# split it into chunks
-# text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-# docs = text_splitter.split_documents(documents)
-print("Document split into chunks successfully.")
 
 # create the open-source embedding function
 embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
@@ -361,50 +212,6 @@ print("Query executed successfully.")
 
 # print results
 print(docs[0].page_content)
-
-
-# In[59]:
-
-
-# # import
-# from langchain_chroma import Chroma
-# from langchain_community.document_loaders import TextLoader
-# from langchain_community.embeddings.sentence_transformer import (
-#     SentenceTransformerEmbeddings,
-# )
-# # from langchain_text_splitters import CharacterTextSplitter
-
-# print("Imports completed successfully.")
-
-# # # load the document and split it into chunks
-# # loader = TextLoader("../../modules/state_of_the_union.txt")
-# # documents = loader.load()
-# print("Document loaded successfully.")
-
-# # # split it into chunks
-# # text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-# # docs = text_splitter.split_documents(documents)
-# print("Document split into chunks successfully.")
-
-# # create the open-source embedding function
-# embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
-# print("Embedding function created successfully.")
-
-# # load it into Chroma
-# db = Chroma.from_documents(documents, embedding_function)
-# print("Documents loaded into Chroma successfully.")
-
-# # query it
-# query = "What did the president say about Ketanji Brown Jackson"
-# docs = db.similarity_search(query)
-# print("Query executed successfully.")
-
-# # print results
-# print(docs[0].page_content)
-
-
-# In[23]:
-
 
 # Call the `similarity_search_with_score` method on `db`
 results = db.similarity_search_with_score("tell me about alzheimer's?")
